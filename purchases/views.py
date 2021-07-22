9da6a3b12f5b262764117f0a78a008a6500adcf2
unknown <carl_mard@hotmail.com>
2021-07-23 01:06:56 +0200

    beta

--- purchases/views.py
+++ purchases/views.py
@@ -1,22 +1,33 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.forms import modelform_factory
 from django.http import HttpResponse
-
 from purchases.models import Item, ItemRelationship, Person
 
 def index(request):
     return render (HttpResponse)
 
 def items(request):
+    
+    
     person = Person.objects.all()
     items = Item.objects.all()
     purchases = ItemRelationship.objects.all()
+            
     context = {"items": items,  "persons": person, "purchases": purchases}
     return render(request, 'purchases/items.html', context)
             
 
 def purchased(request):
+    PurchaseForm = modelform_factory(ItemRelationship, exclude=[])
+    form = PurchaseForm(request.POST)
     person = Person.objects.all()
     purchases = ItemRelationship.objects.all()
-    context = {"purchases": purchases,  "persons": person} 
+    if request.method == "POST":
+        purchase = PurchaseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("purchased")
+        else:
+            form = PurchaseForm()
+    context = {"purchases": purchases,  "persons": person, "form": form} 
     return render(request, 'purchases/purchased.html', context)
