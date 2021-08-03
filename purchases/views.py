60ba7c71c07d7ce91300b3cc85e291bba7a2a782
Carl-Philip Mård <carl-philip.mard@basalt.se>
2021-08-03 12:54:54 +0200

    små ändringar

--- purchases/views.py
+++ purchases/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.forms import modelform_factory
 from django.http import HttpResponse
+from django.templatetags.static import static
 from purchases.models import Item, ItemRelationship, Person
 
 def index(request):
     return render (HttpResponse)
 
 def items(request):
-    
-    
+    default = static('webshop/default.jpg')
     person = Person.objects.all()
     items = Item.objects.all()
     purchases = ItemRelationship.objects.all()
